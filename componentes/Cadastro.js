ec95f399736eec98c2382371a62d5b713380d9ae
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-16 21:37:39 -0300

    add user registration functionality and update comments in Cadastro component

--- componentes/Cadastro.js
+++ componentes/Cadastro.js
@@ -4,17 +4,24 @@ import {View, Text, TextInput, Button, StyleSheet} from 'react-native'
 export default function Cadastro(){
     const [nomeUsuario, setNomeUsuario] = useState()
     const [senha, setSenha] = useState()
+    const [listaUsuarios, setListaUsuarios] = useState()
 
     const cadastrar = () =>{
+// cria uma array de objetos chamada objetos. cada objeto vai conter nomeUsuario e senha
+        const obj = {
+            nomeUsuario, senha
+        }
+//os 3 pontinhos repete o conteúdo que ja tem na array e coloca o obj em sequencia (praticamente um "push")
+        setListaUsuarios([...listaUsuarios, obj])
+
         alert('Cadastrei')
     }
 
-    return(
-        // Tudo isso será mostrado na tela, pois é o que retorna quando eu chamo lá no app.js
+    return( // Tudo aqui dentro será mostrado na tela, pois é o que retorna quando eu chamo lá no app.js
         <View>   
-{/* // ovalue recebe a variavel que criamos no useState (nomeUsuario)
-// o value recebe o método (setUseState)
-//isso é equivalente ao "getElementsByID" */}
+{/* // o value recebe a variavel que criamos no useState (nomeUsuario)
+        o value recebe o método (setUseState)
+        isso é equivalente ao "getElementsByID" */}
             <Text>Cadastro</Text>
             <TextInput placeholder='Nome de usuário'
                 value={nomeUsuario}
