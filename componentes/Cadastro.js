b9c071f0b1aa328bf0c7529fdd725d69bb7eb083
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-16 21:17:25 -0300

    implement user input handling and registration alert in Cadastro component

--- componentes/Cadastro.js
+++ componentes/Cadastro.js
@@ -1,13 +1,33 @@
+import { useState } from 'react'
 import {View, Text, TextInput, Button, StyleSheet} from 'react-native'
+import { useState } from 'react'
 
 export default function Cadastro(){
-    return(
-        <View>
-            <Text>Cadastro</Text>
-            <TextInput placeholder='Nome de usuário'></TextInput>
-            <TextInput placeholder='Senha'></TextInput>
-            <Button title='Cadastrar' color='#555'></Button>
+    const [nomeUsuario, setNomeUsuario] = useState()
+    const [senha, setSenha] = useState()
 
+    const cadastrar = () =>{
+        alert('Cadastrei')
+    }
+
+    return(
+        // Tudo isso será mostrado na tela, pois é o que retorna quando eu chamo lá no app.js
+        <View>   
+            <Text>Cadastro</Text>
+
+            <TextInput placeholder='Nome de usuário'
+                value={nomeUsuario}
+                onChangeText={setNomeUsuario}
+            ></TextInput>
+
+            <TextInput placeholder='Senha'
+                value={senha}
+                onChangeText={setSenha}
+            ></TextInput>
+
+            <Button title='Cadastrar' 
+            color='#555'
+            onPress={cadastrar}></Button>
         </View>
     )
 }
