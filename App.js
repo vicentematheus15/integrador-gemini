4685f764e5bfd9409017a6cff073ceeed08f154d
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-18 19:38:52 -0300

    replace Cadastro component with Login component in App.js

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Cadastro from './componentes/Cadastro';
+import Login from './componentes/Login'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Olá mundo!</Text>
 {/* o cadastro.js aparece na tela porque a função criada lá RETORNA o que deve ser mostrado */}
-      <Cadastro></Cadastro>
+      <Login></Login>
 
       <StatusBar style="auto" />
     </View>
