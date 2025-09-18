ad738d298fe249d09a700c3be6b26e10b4af5f0d
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-18 19:45:10 -0300

    remove unused StatusBar import and update App component to render Login

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Cadastro from './componentes/Cadastro';
 import Login from './componentes/Login'
@@ -6,11 +5,10 @@ import Login from './componentes/Login'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá mundo!</Text>
+
 {/* o cadastro.js aparece na tela porque a função criada lá RETORNA o que deve ser mostrado */}
       <Login></Login>
 
-      <StatusBar style="auto" />
     </View>
   );
 }
