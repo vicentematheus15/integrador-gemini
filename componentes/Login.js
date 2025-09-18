74e2246c58a77576f928d79b35d2309002eba335
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-18 20:01:04 -0300

    add state management for username and password in Login component

--- componentes/Login.js
+++ componentes/Login.js
@@ -1,10 +1,29 @@
 import {View, Text, Button, TextInput, StyleSheet} from 'react-native'
+import { useState } from 'react'
 
 export default function Login() {
+    const [nomeUsuario, setNomeUsuario] = useState()
+    const [senha, setSenha] = useState()
+
 
     return(
         <View>
             <Text>Login</Text>
+
+            <TextInput placeholder='Nome de Usuário'
+                value={nomeUsuario}
+                onChangeText={setNomeUsuario}
+                
+
+            ></TextInput>
+
+            <TextInput placeholder='Senha'
+                value={senha}
+                onChangeText={setSenha}
+                secureTextEntry
+            
+            ></TextInput>
+
 
         </View>
     )
