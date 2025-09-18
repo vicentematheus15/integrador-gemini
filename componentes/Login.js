ce6940ddde09e7b901380d6a152f2c3c4cace4de
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-18 19:39:08 -0300

    add Login component header text

--- componentes/Login.js
+++ componentes/Login.js
@@ -1,10 +1,10 @@
-
+import {View, Text, Button, TextInput, StyleSheet} from 'react-native'
 
 export default function Login() {
 
     return(
         <View>
-
+            <Text>Login</Text>
 
         </View>
     )
