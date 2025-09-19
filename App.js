69d7f27730945dbac306eafafac5f5304021916b
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-18 21:29:01 -0300

    add: implement Gemini component with prompt input and response handling

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Cadastro from './componentes/Cadastro';
 import Login from './componentes/Login'
+import Gemini from './componentes/Gemini'
 
 export default function App() {
   return (
     <View style={styles.container}>
 
 {/* o cadastro.js aparece na tela porque a função criada lá RETORNA o que deve ser mostrado */}
-      <Login></Login>
+      <Gemini></Gemini>
 
     </View>
   );
--- componentes/Gemini.js
+++ componentes/Gemini.js
@@ -1,11 +1,49 @@
-import { View, Text, TextInput, StyleSheet, ScrollView } from 'react-native'
+import { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, ScrollView, Button } from 'react-native'
 
 export default function Gemini () {
+const [prompt, setPrompt] = useState();
+const [resposta, setResposta] = useState();
+
+const enviarPrompt = async () => {
+    const apiKey = 'AIzaSyAfSv6vU1KhdLkot5MT0mLw5j4Hzs6L5os';
+    const ulr = ``;
+
+    const resposta = await fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'aplication/json'},
+        body: {
+            "contents": [
+              {
+                "parts": [
+                  {
+                    "text": "Explain how AI works in a few words"
+                  }
+                ]
+              }
+            ]
+          }
+    })
+}
 
     return(
         <View>
             <Text>
                 Gemini
+            </Text>
+
+            <TextInput placeholder='Prompt ao Gemini'
+                value={prompt}
+                onChangeText={setPrompt}            
+            ></TextInput>
+
+            <Button onPress={enviarPrompt}
+            title='Enviar'
+            color='#555'
+            ></Button>
+
+            <Text>
+                {resposta}
             </Text>
         </View>
     )
