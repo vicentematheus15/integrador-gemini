d1bbfc3c46be422177a8fd601a55a2145327e61e
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-18 21:36:16 -0300

    fix: update Gemini component to correct API URL and improve request body structure

--- componentes/Gemini.js
+++ componentes/Gemini.js
@@ -7,23 +7,29 @@ const [resposta, setResposta] = useState();
 
 const enviarPrompt = async () => {
     const apiKey = 'AIzaSyAfSv6vU1KhdLkot5MT0mLw5j4Hzs6L5os';
-    const ulr = ``;
+    const ulr = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
     const resposta = await fetch(url, {
         method: 'POST',
         headers: {'Content-Type': 'aplication/json'},
-        body: {
-            "contents": [
-              {
-                "parts": [
+        body: JSON.stringify(
+            {
+                "contents": [
                   {
-                    "text": "Explain how AI works in a few words"
+                    "parts": [
+                      {
+                        "text": prompt
+                      }
+                    ]
                   }
                 ]
               }
-            ]
-          }
+        )
     })
+
+    const extraido = await resposta.json();
+    const text = extraido.candidates[0].content.parts[0].text;
+    setResposta(text)
 }
 
     return(
